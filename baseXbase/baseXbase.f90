c86e3caca1344f23241684c5e3e24b780b623579
Marco Nardes <marconardes@gmail.com>
2017-03-20 11:01:38 -0300

    modified:   baseXbase/baseXbase.f90

--- baseXbase/baseXbase.f90
+++ baseXbase/baseXbase.f90
@@ -1,5 +1,5 @@
  program baseXbase
-   integer            :: baseAtual=2, baseConvertida=10, length=0, c =1, toInt = 0
+   integer            :: baseAtual=2, baseConvertida=10, length=0, c =1, toInt = 0, result10=0, presult=0
    character(len=100) :: numberToConvert ="100"
    character          :: var 
 	
@@ -10,7 +10,11 @@
       do c = 1, length, 1
 	var = numberToConvert(c:c)
 	read(var,'(I10)') toInt
-	write(*,*) toInt
+	write(*,*) toInt,length-c
+        presult = toInt*(baseAtual**(length-c))
+	write(*,*) presult
+	result10 = result10 + presult
+        write(*,*)result10
       end do
    else 
       write(*,*)"A base atual é ",baseAtual
